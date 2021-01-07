8ce1226b54f1de922f3034a33cf5fe9cf10d26e0
David <davidcondori99@gmail.com>
2021-01-07 13:52:10 -0500

    share code in github

---
+++ frontend/src/data.js
@@ -0,0 +1,72 @@
+const data = {
+    products:[
+        {
+            _id: '1',
+            name: 'Nike Slim Shirt',
+            category: 'Shirts',
+            image: '/images/p1.jpg',
+            price: 120,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'producto de alta calidad'
+        },
+        {
+            _id: '2',
+            name: 'Adidas Slim Shirt',
+            category: 'Shirts',
+            image: '/images/p2.jpg',
+            price: 112,
+            brand: 'Adidas',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'producto de alta calidad'
+        },
+        {
+            _id: '3',
+            name: 'Puma Slim Shirt',
+            category: 'Pants',
+            image: '/images/p3.jpg',
+            price: 65,
+            brand: 'Puma',
+            rating: 4.9,
+            numReviews: 10,
+            description: 'producto de alta calidad'
+        },
+        {
+            _id: '4',
+            name: 'Lomas Slim Pant',
+            category: 'Shirts',
+            image: '/images/p4.jpg',
+            price: 120,
+            brand: 'Lomas',
+            rating: 4.8,
+            numReviews: 14,
+            description: 'producto de alta calidad'
+        },
+        {
+            _id: '5',
+            name: 'Rebook Slim Pant',
+            category: 'Shirts',
+            image: '/images/p5.jpg',
+            price: 139,
+            brand: 'Rebook',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'producto de alta calidad'
+        },
+        {
+            _id: '6',
+            name: 'Rebook Slim Pant',
+            category: 'Shirts',
+            image: '/images/p6.jpg',
+            price: 139,
+            brand: 'Rebook',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'producto de alta calidad'
+        },
+    ]
+}
+
+export default data;
